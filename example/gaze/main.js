3ea4ecd0e34c9813d5234bb76c9069201899bc28
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-06-03 10:32:57 -0400

    update

--- example/gaze/main.js
+++ example/gaze/main.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 const text = document.getElementById('gaze-position')
+let startingStamp, averagingX, averagingY, averagingCount
 
 function go() {
   document.getElementById('start-button').style.display = 'none'
@@ -8,10 +9,32 @@ function go() {
   RemoteCalibrator.trackGaze(
     {
       fullscreen: false,
-      calibrationCount: 7,
+      calibrationCount: 1,
     },
     data => {
-      text.innerHTML = data.value.x + 'px, ' + data.value.y + 'px'
+      if (!startingStamp) {
+        startingStamp = data.timestamp
+        averagingX = averagingY = averagingCount = 0
+      }
+
+      if (data.timestamp - startingStamp < 100) {
+        averagingX += data.value.x
+        averagingY += data.value.y
+        averagingCount++
+      } else {
+        text.innerHTML =
+          Math.round((10 * averagingX) / averagingCount) / 10 +
+          'px, ' +
+          Math.round((10 * averagingY) / averagingCount) / 10 +
+          'px  ' +
+          `${data.timestamp.getHours()}:${data.timestamp.getMinutes()}:${data.timestamp.getSeconds()}.${data.timestamp.getMilliseconds()}`
+
+        startingStamp = data.timestamp
+        averagingX = averagingY = averagingCount = 0
+        averagingX += data.value.x
+        averagingY += data.value.y
+        averagingCount++
+      }
     }
   )
 }
