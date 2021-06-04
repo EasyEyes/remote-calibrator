1bb428591d95a45d61993643732d0c7880495092
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-06-04 00:42:43 -0400

    add ppi display

--- example/gaze/main.js
+++ example/gaze/main.js
@@ -25,7 +25,7 @@ document.getElementById('code-degree').innerHTML = `QR Codes X &#177;${
   Math.round(codeToCenterXDEG * 1000000) / 1000000
 }deg, Y &#177;${
   Math.round(codeToCenterYDEG * 1000000) / 1000000
-}deg (27in screen, 40cm viewing distance, fullscreen)`
+}deg (27in screen, 40cm viewing distance, fullscreen) | PPI ${ppc * 2.54}`
 
 /* -------------------------------------------------------------------------- */
 const gazeData = []
