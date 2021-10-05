67d871a8001d9b45a4ab88db92a16ea7bd37dd7a
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-10-04 20:55:43 -0400

    reduce feedback sound

--- src/components/sound.js
+++ src/components/sound.js
@@ -1,11 +1,11 @@
-import { Synth } from 'tone'
+import { Synth, Volume } from 'tone'
 
 const feedbackSynth = new Synth({
   oscillator: {
     type: 'sine',
   },
   envelope: { attack: 0.001, decay: 0.001, sustain: 1, release: 0.001 },
-}).toDestination()
+}).connect(new Volume(-17).toDestination())
 
 export const soundFeedback = () => {
   feedbackSynth.triggerAttackRelease(2000, 0.05)
