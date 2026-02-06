039452625a024243d6660c4d110eee3319791d9a
khahani <mr.khahani@gmail.com>
2026-02-06 16:11:09 +0330

    make checkPermission method reusable

--- src/components/mediaPermission.js
+++ src/components/mediaPermission.js
@@ -5,23 +5,19 @@ import { swalInfoOptions } from './swalOptions'
 
 import AllowCam from '../media/allow-camera.png?width=240&height=120'
 
-export const checkPermissions = async RC => {
+export const checkPermissions = async (RC, message) => {
   if (navigator.permissions?.query) {
     return navigator.permissions
       .query({ name: 'camera' })
       .then(async permissionObj => {
         if (permissionObj.state === 'prompt') {
-          let html = `${phrases.RC_requestCamera[RC.L]}`
-          if (!RC.options.saveSnapshots) {
-            html += `<br />${phrases.RC_privacyCamera[RC.L]}`
-          }
           return await Swal.fire({
             ...swalInfoOptions(RC, { showIcon: false }),
             icon: undefined,
             imageUrl: AllowCam,
             imageWidth: 480,
             imageAlt: 'Please allow camera access',
-            html: html,
+            html: message,
           })
         }
       })
