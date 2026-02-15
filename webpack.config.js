89294c1ac1f34d0be0500855194407698601f5f7
khahani <mr.khahani@gmail.com>
2026-02-15 20:35:38 +0330

    configure Access-Control-Allow-* for dev server

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,11 @@ const exampleConfig = Object.assign({}, config, {
     port: 9000,
     hot: true,
     open: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
   },
   plugins: plugins,
 })
